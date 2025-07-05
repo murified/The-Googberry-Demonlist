7a9a01b2bb480837c75b1c69d905f1f62c11d31a
vorvexy <bentlyes343@gmail.com>
2025-07-05 17:21:26 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,22 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Must be 30 seconds or higher
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Must have some form of decoration
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Must not have any secret ways unless its a secret way that kills you
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Must not have any skippable parts
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        Must be runnable on PC and Mobile
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Must be possible on a minimum of 60 fps
                     </p>
                 </div>
             </div>
